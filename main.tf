9d1f2ef01fdb08be49a2c37190dd97bb2962220c
LiwaaHosh <66652532+LiwaaCoder@users.noreply.github.com>
2023-02-25 20:50:36 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,36 +1,108 @@
 provider "google" {
-  project     = "delta-geode-378123"
-  credentials = file("/Users/liwaa/Downloads/delta-geode-378123-8f94edbe2a6e.json")
+  project     = "wideops-challlenge-dev"
+  credentials = file("/Users/liwaa/Downloads/wideops-challlenge-dev-39f036c669e3.json")
   region      = "us-central1"
 }
 
+
+resource "google_sql_database_instance" "wordpress" {
+  name             = "wordpress"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "wordpress" {
+  name     = "wordpress"
+  instance = google_sql_database_instance.wordpress.name
+
+  charset    = "utf8mb4"
+  collation  = "utf8mb4_general_ci"
+
+  depends_on = [google_sql_database_instance.wordpress]
+}
+
+
+
+resource "random_string" "username" {
+  length  = 10
+  special = false
+}
+
+resource "random_password" "password" {
+  length  = 16
+  special = true
+}
+
+output "db_name" {
+  value = google_sql_database.wordpress.name
+}
+
+output "db_username" {
+  value = random_string.username.result
+}
+
+output "db_password" {
+
+  value = random_password.password.result
+  sensitive = true
+}
+
+
+
+resource "google_compute_network" "vpc" {
+  name                    = "my-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "vpc-subnet" {
+  name          = "my-vpc-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = google_compute_network.vpc.self_link
+}
+
+
 provider "kubernetes" {
-  host                   = "https://35.202.183.17"
-  config_path            = "~/.kube/config"
-  config_context_cluster = "gke_delta-geode-378123_us-central1_autopilot-cluster-wideops-12"
-  token                  = data.google_client_config.access_token.access_token
+  config_path = "~/.kube/config"
 }
 
-data "google_client_config" "access_token" {}
 
 
-resource "google_container_cluster" "autopilot" {
-  name                  = "autopilot-cluster-wideops-12"
-  location              = "us-central1"
-  remove_default_node_pool = true
-  initial_node_count    = 1
+resource "google_container_cluster" "wordpress" {
+  name               = "wordpress"
+  location           = "us-central1"
+  initial_node_count = 2
+  network            = google_compute_network.vpc.name
+  subnetwork         = google_compute_subnetwork.vpc-subnet.self_link # added subnetwork
+
+  node_config {
+    machine_type = "n1-standard-1"
+    disk_size_gb = 10
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
 }
+
+
+
 
 
 
 resource "kubernetes_deployment" "wordpress" {
   metadata {
-    name      = "wordpress"
-    namespace = "default"
+    name = "wordpress"
   }
 
   spec {
-    replicas = 4
+    replicas = 2
 
     selector {
       match_labels = {
@@ -47,61 +119,38 @@ resource "kubernetes_deployment" "wordpress" {
 
       spec {
         container {
-          name  = "wordpress"
           image = "wordpress:latest"
-
-          port {
-            container_port = 80
-          }
+          name  = "wordpress"
 
           env {
             name  = "WORDPRESS_DB_HOST"
-            value = "mongo-service"
+            value = google_sql_database_instance.wordpress.connection_name
+
           }
 
           env {
-            name = "WORDPRESS_DB_USER"
-            value_from {
-              secret_key_ref {
-                name = "mongo-secret"
-                key  = "mongo-user"
-              }
-            }
+            name  = "WORDPRESS_DB_USER"
+            value = "root"
           }
 
           env {
-            name = "WORDPRESS_DB_PASSWORD"
-            value_from {
-              secret_key_ref {
-                name = "mongo-secret"
-                key  = "mongo-password"
-              }
-            }
+            name  = "WORDPRESS_DB_PASSWORD"
+            value = "sqlsql123"
+          }
+
+          env {
+            name  = "WORDPRESS_DB_NAME"
+            value = google_sql_database.wordpress.name
+          }
+
+          port {
+            container_port = 80
           }
         }
       }
     }
   }
 }
-
-
-resource "kubernetes_service" "mongodb" {
-  metadata {
-    name = "mongodb-service"
-    namespace = "default"
-  }
-  spec {
-    selector = {
-      app = "mongodb"
-    }
-    port {
-      name = "mongodb"
-      port = 27017
-      target_port = 27017
-    }
-  }
-}
-
 
 
 resource "kubernetes_service" "wordpress" {
@@ -128,48 +177,22 @@ resource "kubernetes_ingress" "wordpress" {
   metadata {
     name = "wordpress"
     annotations = {
-      "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
-      "kubernetes.io/ingress.class" = "nginx"
-      "nginx.ingress.kubernetes.io/ssl-redirect" = "true"
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
     }
   }
 
   spec {
-    tls {
-      secret_name = "wordpress-tls"
-      hosts       = ["liwaadev.com"]
-    }
-
     rule {
-      host = "liwaadev.com"
-
       http {
         path {
+          backend {
+            service_name = kubernetes_service.wordpress.metadata[0].name
+            service_port = 80
+          }
           path = "/"
-          backend {
-            service_name = kubernetes_service.wordpress.metadata[0].name
-            service_port = 80
-          }
-        }
-
-        path {
-          path = "/api"
-          backend {
-            service_name = kubernetes_service.wordpress.metadata[0].name
-            service_port = 80
-          }
-        }
-
-        path {
-          path = "/mongodb"
-          backend {
-            service_name = kubernetes_service.mongodb.metadata[0].name
-            service_port = 27017
-          }
         }
       }
     }
   }
 }
-
 
